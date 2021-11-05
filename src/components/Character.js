d2ee22126052f95008bbd9250d485cfedefc2617
Ryan Howard <ryanghoward@protonmail.com>
2021-11-05 10:17:10 -0700

    Character component underway

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,14 @@
+import React from 'react';
 // Write your Character component here
+
+const Character = (props) => {
+    const { characters } = props;
+
+    return (
+        <div>
+            {characters.map((res.index) => {
+                
+            })}
+        </div>
+    )
+}
