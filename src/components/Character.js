130aa9d876c04bd8e303846916398484622974cd
Ryan Howard <ryanghoward@protonmail.com>
2021-11-05 12:16:34 -0700

    Working on additional info drop down functionality

--- src/components/Character.js
+++ src/components/Character.js
@@ -5,6 +5,7 @@ const CharacterCard = styled.div`
     background-color: #2b3618;
     border: 1px solid lime;
     width: 50vw;
+    margin: 1em auto;
     opacity: 75%;
 `
 
@@ -19,15 +20,22 @@ const StyledH2 = styled.h2`
 //~ Write your Character component here
 
 const Character = (props) => {
-    const { characters } = props;
+    const { characters, films, actionFilms } = props;
 
     return (
         <div>
             {characters.map((res, index) => {
                 return (
                     <CharacterCard key={index}>
-                    <StyledName>{res.name}</StyledName>
-                    <StyledH2>{res.gender}</StyledH2>
+                    <div>
+                        <StyledName>{res.name}</StyledName>
+                        <StyledH2>Gender: {res.gender}</StyledH2>
+                        <StyledH2>Birth Year: {res.birth_year}</StyledH2>
+                    </div>
+                    <div>
+                    {/* <StyledH2><a href={res.homeworld}>Home Planet</a></StyledH2> */}
+                    <button onClick={() => actionFilms(films)}>{res.homeworld}</button>
+                    </div>
                     </CharacterCard>
                 )
             })}
