7d1d54b14e937fa1d9a60b09c5e7f569f988f203
Ryan Howard <ryanghoward@protonmail.com>
2021-11-05 09:56:18 -0700

    Datais being recieved

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 const App = () => {
+  const [characterData, setCharacterData] = useState([]);
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+
+  useEffect (() => {
+    axios.get('https://swapi.dev/api/people')
+      .then(res => {
+        console.log(res.data);
+      })
+  })
 
   return (
     <div className="App">
