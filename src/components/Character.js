acb24c0c9775ffb5abad4bf3edd1bbc4e628a79b
Ryan Howard <ryanghoward@protonmail.com>
2021-11-05 10:29:26 -0700

    Started building styled components

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,5 +1,20 @@
 import React from 'react';
-// Write your Character component here
+import styled from 'styled-components';
+
+const CharacterCard = styled.div`
+    background-color: black;
+    border: 1px solid red;
+`
+
+const StyledName = styled.h1`
+    color: white;
+`
+
+const StyledH2 = styled.h2`
+    color: white;
+`
+
+//~ Write your Character component here
 
 const Character = (props) => {
     const { characters } = props;
@@ -7,7 +22,10 @@ const Character = (props) => {
     return (
         <div>
             {characters.map((res.index) => {
-                
+                <CharacterCard key={index}>
+                    <StyledName>{res.name}</StyledName>
+                    <StyledH2>{res.gender}</StyledH2>
+                </CharacterCard>
             })}
         </div>
     )
